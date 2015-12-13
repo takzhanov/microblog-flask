5da7a9666f89125c68fb6f99fed2fb7beacb30bc
ytakzhanov <takzhanov@gmail.com>
2015-12-13 16:39:18 +0300

    les 5.1

--- app/views.py
+++ app/views.py
@@ -70,3 +70,9 @@ def after_login(resp):
 @app.before_request
 def before_request():
     g.user = current_user
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
