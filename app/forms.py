3149e84604e3e2d5ed1d3c4626f1391670c0a84f
ytakzhanov <takzhanov@gmail.com>
2015-12-14 01:25:23 +0300

    les 6

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import TextField, StringField, BooleanField, TextAreaField
+from wtforms.validators import Required, DataRequired, Length
 
 
 class LoginForm(Form):
     openid = TextField('openid', validators=[Required()])
     remember_me = BooleanField('remember_me', default=False)
+
+
+class EditForm(Form):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
